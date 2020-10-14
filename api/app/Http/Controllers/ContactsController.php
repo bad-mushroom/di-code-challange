b76b76289a0af38c18884323f842f9a811b54822
Chris Sprague <chris@chaoscontrol.org>
2020-10-14 19:39:05 -0400

    Create a contact and return 201

--- api/app/Http/Controllers/ContactsController.php
+++ api/app/Http/Controllers/ContactsController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
 class ContactsController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
+        $contact = Contact::create($request->all());
 
+        return response($contact, 201);
     }
 }
